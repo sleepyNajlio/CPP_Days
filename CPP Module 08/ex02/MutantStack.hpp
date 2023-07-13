ae644d3d15b0e0a7d327a6aab6df9f3da1bc051f
Najlaa Loutfi <nloutfi@e1r3p4.1337.ma>
2023-07-13 07:57:31 +0100

    08 almost done

--- CPP Module 08/ex02/MutantStack.hpp
+++ CPP Module 08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: nloutfi <nloutfi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 06:48:00 by nloutfi           #+#    #+#             */
-/*   Updated: 2023/07/13 07:35:49 by nloutfi          ###   ########.fr       */
+/*   Updated: 2023/07/13 07:47:50 by nloutfi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,11 @@ class MutantStack : public std::stack<T>
         MutantStack(MutantStack const & src) : std::stack<T>(src) {}
         virtual ~MutantStack() {}
         MutantStack & operator=(MutantStack const & rhs) { std::stack<T>::operator=(rhs); return *this; }
-
+        
         typedef typename std::stack<T>::container_type::iterator iterator;
+        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
         iterator begin() { return std::stack<T>::c.begin(); }
         iterator end() { return std::stack<T>::c.end(); }
+        const_iterator begin() const { return std::stack<T>::c.begin(); }
+        const_iterator end() const { return std::stack<T>::c.end(); }
 };
